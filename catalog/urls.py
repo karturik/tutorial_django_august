4be9eac10ff3dcb9ce284829b311a0763d76d94d
karturik <kazukevich.artur@gmail.com>
2025-10-04 10:38:44 +0200

    add book list view and template

--- catalog/urls.py
+++ catalog/urls.py
@@ -1,7 +1,9 @@
-from django.urls import path, include
+from django.urls import path, re_path, include
 import catalog.views as views
 
 
+
 urlpatterns = [
-    path('', views.index, name='index')
+    path('', views.index, name='index'),
+    re_path(r'^books/$', views.BookListView.as_view(), name='books')
 ]
--- catalog/views.py
+++ catalog/views.py
@@ -1,8 +1,13 @@
+from typing import Any
+from django.db.models.query import QuerySet
+from django.views import generic
 from django.shortcuts import render
+from django.http.response import HttpResponse
+
 from .models import Book, Author, BookInstance, Genre
 
 # Create your views here.
-def index(request):
+def index(request) -> HttpResponse:
     """
     Home page view function
     """
@@ -26,3 +31,18 @@ def index(request):
         'index.html',
         context=context
     )
+
+class BookListView(generic.ListView):
+    model = Book
+    context_object_name = 'my_book_list'
+    queryset = Book.objects.filter(title__icontains='war')[:5]
+    template_name = 'books/book_list_page.html'
+
+    # def get_queryset(self) -> QuerySet[Any]:
+    #     return Book.objects.filter(title__icontains='war')[:5]
+
+    def get_context_data(self, **kwargs: Any) -> dict[str, Any]:
+        context = super(BookListView, self).get_context_data(**kwargs)
+
+        context['some_data'] = 'This is just some data'
+        return context
