77c0c177a74553e37fa608d221284cfbf967b6b0
karturik <kazukevich.artur@gmail.com>
2025-11-01 08:21:14 +0100

    finish ajax

--- users_and_accounts/models.py
+++ users_and_accounts/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
-# Create your models here.
+class ContactMessage(models.Model):
+    name = models.CharField(max_length=100)
+    email = models.EmailField()
+    message = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True) # Дата и время создания
+
+    def __str__(self):
+        return f"Message from {self.name} ({self.email})"
+
+    class Meta:
+        verbose_name = "Contact message"
+        verbose_name_plural = "Contact messages"
+        ordering = ['-created_at']
